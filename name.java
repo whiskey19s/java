4fbcdd9c6f379bb3d707c83f18a9c6e0f48b4583
whiskey19s <21311042su@tama.ac.jp>
2014-10-08 16:00:31 +0900

    第3回目

---
+++ name.java
@@ -0,0 +1,11 @@
+class name
+{
+    public static void main(String argv[])
+    {
+		String name;
+	    System.out.println("‚ ‚È‚½‚Ì–¼‘O‚ÍH");
+		System.out.println("name=");
+		name = new java.util.Scanner(System.in).nextLine();
+		 System.out.println(name+"‚³‚ñA‚±‚ñ‚É‚¿‚Í");
+    }
+}
